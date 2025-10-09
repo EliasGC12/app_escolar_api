89cdeab71dcbefa16e02ba4e9525bbb3cfad15cc
Luis Yael Mendez Sanchez <luisyaelms@gmail.com>
2025-10-08 18:30:47 -0600

    Initial commit

---
+++ app_escolar_api/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .views.bootstrap import VersionView
+from app_escolar_api.views import bootstrap
+from app_escolar_api.views import users
+from app_escolar_api.views import auth
+
+
+urlpatterns = [
+    #Create Admin
+        path('admin/', users.AdminView.as_view()),
+    #Admin Data
+        path('lista-admins/', users.AdminAll.as_view()),
+    #Edit Admin
+        #path('admins-edit/', users.AdminsViewEdit.as_view())
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
